7badbf1eb291919905c86f602e8d0136fb14bab5
x-zho14 <18813119938@163.com>
2021-10-18 22:48:02 +0800

    Update swin_transformer.py

--- models/swin_transformer.py
+++ models/swin_transformer.py
@@ -1,4 +1,4 @@
--# --------------------------------------------------------
+# --------------------------------------------------------
 # Swin Transformer
 # Copyright (c) 2021 Microsoft
 # Licensed under The MIT License [see LICENSE for details]
