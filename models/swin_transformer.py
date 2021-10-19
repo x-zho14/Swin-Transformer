790f745eaba4eedde45cfe1a2acfe55d4b96e3bb
x-zho14 <18813119938@163.com>
2021-10-19 15:37:09 +0800

    Update swin_transformer.py

--- models/swin_transformer.py
+++ models/swin_transformer.py
@@ -203,7 +203,7 @@ class SwinTransformerBlock(nn.Module):
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.mlp = Mlp(builder, in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
 
         if self.shift_size > 0:
             # calculate attention mask for SW-MSA
